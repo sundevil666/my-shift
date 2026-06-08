c99a63251d80a9318afc577b89e01333935a0f5b
sundevil <sundevildi@gmail.com>
2026-06-08 12:23:47 +0200

    feat(i18n): add localized application messages

---
+++ src/i18n/index.ts
@@ -0,0 +1,11 @@
+import enUS from './en-US';
+import ruRU from './ru-RU';
+import ukUA from './uk-UA';
+import skSK from './sk-SK';
+
+export default {
+  'en-US': enUS,
+  'ru-RU': ruRU,
+  'uk-UA': ukUA,
+  'sk-SK': skSK,
+};
